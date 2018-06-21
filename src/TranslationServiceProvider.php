0214490f280e5a97e74817dc64a337003fec917f
Tim Joosten <Tjoosten3@gmail.com>
2018-06-21 20:11:03 +0200

    Initial commit

---
+++ src/TranslationServiceProvider.php
@@ -0,0 +1,29 @@
+<?php 
+
+namespace Misfits\Translations;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Class TranslationServiceProvider 
+ * ---- 
+ * The service provider for our translation package that is used accross the platforms
+ * from the Activisme_BE organisation. 
+ * 
+ * @author      Tim Joosten <tim@activisme.be>
+ * @copyright   Tim Joosten <MIT license>
+ * @package     Misfits\Translations
+ */
+class TranslationServiceProvider extends ServiceProvider
+{
+    /** 
+     * Bootstrap any application services 
+     * 
+     * @return void
+     */
+    public function boot(): void
+    {
+        // USAGE: __('starter::<group>.<key>')
+        $this->loadTranslationsFrom(__DIR__ . '/resources/trans', 'starter');
+    }
+}
---
+++ src/resources/trans/en/database.php
@@ -0,0 +1,33 @@
+<?php 
+
+return [
+
+    /**
+     * -----------------------------------------------------------------------
+     * Database Translations Lines 
+     * -----------------------------------------------------------------------
+     * 
+     * The following language lines are used during the database seeds for 
+     * various messages that we need to display to the user. You are free 
+     * to modify these language lines according to your application's 
+     * requirements. 
+     * 
+     */
+
+    // DatabaseSeeder.php 
+    'ask-refresh'               => 'Do you wish to refresh the migration before seeding, it will clear all old data?', 
+    'cleared-database'          => 'Data cleared, starting from blank database.', 
+
+    // usersTableSeeder.php
+    'permissions-added'         => '', 
+    'ask-roles-default'         => '', 
+    'ask-roles-user'            => '', 
+    'perms-synced-admin'        => '', 
+    'role-created-info'         => '',
+    'only-default-role-added'   => '', 
+    'no-roles-added'            => '',
+    'user-admin-role'           => '', 
+    'default-password'          => 'Password is "secret"',
+
+
+];
