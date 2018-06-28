b12ebdeede74ac405c2f4fb33fa0cc051246fb31
Tim Joosten <Tjoosten3@gmail.com>
2018-06-28 23:05:48 +0200

    Implement fragments index translations

---
+++ src/resources/trans/en/fragments.php
@@ -0,0 +1,35 @@
+<?php 
+
+return [
+
+    /**
+     * -----------------------------------------------------------------------
+     * Fragments Translations Lines 
+     * -----------------------------------------------------------------------
+     * 
+     * The following language lines are used during the page fragments system for 
+     * various messages that we need to display to the user. You are free 
+     * to modify these language lines according to your application's 
+     * requirements. 
+     * 
+     */
+
+    'titles' => [
+        'overview' => 'Page fragments overview'
+    ],
+
+    'table' => [
+        'last-editor' => 'Last editor', 
+        'page' => 'Page', 
+        'title' => 'Page title', 
+        'last-edited' => 'Last edited',
+        'editor-unknown' => 'Unknown',
+        'no-results' => 'There are no page fragments found in the application.', 
+
+        'buttons' => [
+            'edit' => 'edit', 
+            'view' => 'view'
+        ],
+    ],
+
+];
